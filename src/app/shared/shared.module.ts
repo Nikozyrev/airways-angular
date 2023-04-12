dcf37cd8e7fdd9f973425a7be9a9c4d9bc1cda9a
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-04-12 16:24:29 +0300

    feat: add angular material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+
+const materialModules = [MatButtonModule];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, ...materialModules],
+  exports: [...materialModules],
 })
 export class SharedModule {}
