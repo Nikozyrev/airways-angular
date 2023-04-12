3faba9ec79fb7311fc30a78b03f57f5b9f85ffee
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-04-12 14:59:08 +0300

    feat: add ngrx

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 })
 export class CoreModule {}
