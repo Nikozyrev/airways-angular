0ff845850688d2afb904a00eb58abc18e9416ec4
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-04-13 21:16:02 +0300

    feat: add flight search module

---
+++ src/app/flight-search/pages/flight-search-page/flight-search-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-search-page',
+  templateUrl: './flight-search-page.component.html',
+  styleUrls: ['./flight-search-page.component.scss'],
+})
+export class FlightSearchPageComponent {}
