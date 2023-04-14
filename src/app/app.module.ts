73571873cb9373791bc87b8c0948a7abf36992e4
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-04-14 11:45:43 +0300

    feat: add footer module and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { FooterModule } from './footer/footer.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { CoreModule } from './core/core.module';
     BrowserAnimationsModule,
     AppRoutingModule,
     CoreModule,
+    FooterModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
