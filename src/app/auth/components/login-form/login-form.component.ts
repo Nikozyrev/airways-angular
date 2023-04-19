c8da9b16aa941b9cb9cca64a8bfa81c6043bad16
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-04-19 20:42:02 +0300

    refactor: change forms to use separate components

--- src/app/auth/components/login-form/login-form.component.ts
+++ src/app/auth/components/login-form/login-form.component.ts
@@ -7,38 +7,12 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent implements OnInit {
-  public hidePassword = true;
-
   public loginForm!: ReturnType<typeof this.createLoginForm>;
 
   constructor(private formBuilder: FormBuilder) {}
 
   public onSubmit(e: SubmitEvent) {
     e.preventDefault();
-  }
-
-  get email() {
-    return this.loginForm.controls.email;
-  }
-
-  get password() {
-    return this.loginForm.controls.password;
-  }
-
-  public getEmailErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  public getPasswordErrorMessage() {
-    if (this.password.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return '';
   }
 
   public ngOnInit() {
--- src/app/auth/components/sign-up-form/sign-up-form.component.ts
+++ src/app/auth/components/sign-up-form/sign-up-form.component.ts
@@ -7,38 +7,12 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./sign-up-form.component.scss'],
 })
 export class SignUpFormComponent implements OnInit {
-  public hidePassword = true;
-
   public signUpForm!: ReturnType<typeof this.createSignUpForm>;
 
   constructor(private formBuilder: FormBuilder) {}
 
   public onSubmit(e: SubmitEvent) {
     e.preventDefault();
-  }
-
-  get email() {
-    return this.signUpForm.controls.email;
-  }
-
-  get password() {
-    return this.signUpForm.controls.password;
-  }
-
-  public getEmailErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  public getPasswordErrorMessage() {
-    if (this.password.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return '';
   }
 
   public ngOnInit() {
