ff13587476c3705699d558bd0fce003027b35c38
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-04-29 21:39:02 +0300

    refactor: create methods for auth effects

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, map, mergeMap, of } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import * as AuthActions from './actions';
 import { AuthModalService } from '../services/auth-modal.service';
 import { AlertService } from '../../core/services/alert.service';
+import { IUser } from '../models/user.model';
 
 @Injectable()
 export class AuthEffects {
@@ -13,16 +15,8 @@ export class AuthEffects {
       ofType(AuthActions.signUp),
       mergeMap(({ userData }) =>
         this.authService.signUp(userData).pipe(
-          map(({ user }) => {
-            this.authModalService.closeDialog();
-            return AuthActions.setUser({ user });
-          }),
-          catchError((error) => {
-            const err =
-              error.status === 0 ? `Server is unavailable` : error.error;
-            this.alertService.showAlert(err);
-            return of(AuthActions.setError({ error: err }));
-          })
+          map(({ user }) => this.handleSuccessAuth(user)),
+          catchError((error) => this.handleError(error))
         )
       )
     );
@@ -33,16 +27,8 @@ export class AuthEffects {
       ofType(AuthActions.login),
       mergeMap(({ userData }) =>
         this.authService.login(userData).pipe(
-          map(({ user }) => {
-            this.authModalService.closeDialog();
-            return AuthActions.setUser({ user });
-          }),
-          catchError((error) => {
-            const err =
-              error.status === 0 ? `Server is unavailable` : error.error;
-            this.alertService.showAlert(err);
-            return of(AuthActions.setError({ error: err }));
-          })
+          map(({ user }) => this.handleSuccessAuth(user)),
+          catchError((error) => this.handleError(error))
         )
       )
     );
@@ -54,4 +40,16 @@ export class AuthEffects {
     private authModalService: AuthModalService,
     private alertService: AlertService
   ) {}
+
+  private handleSuccessAuth(user: IUser) {
+    this.alertService.showAlert('You have logged in');
+    this.authModalService.closeDialog();
+    return AuthActions.setUser({ user });
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    const err = error.status === 0 ? `Server is unavailable` : error.error;
+    this.alertService.showAlert(err);
+    return of(AuthActions.setError({ error: err }));
+  }
 }
