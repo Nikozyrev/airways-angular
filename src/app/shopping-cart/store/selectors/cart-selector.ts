b820c738db3c011ec7b8577fecaec123e2c53c53
Stanislav Gutsev <gutsstas@mail.ru>
2023-05-18 11:06:41 +0300

    fix: remove console.log and comment

--- src/app/shopping-cart/store/selectors/cart-selector.ts
+++ src/app/shopping-cart/store/selectors/cart-selector.ts
@@ -1,5 +1,4 @@
 import { createFeatureSelector } from '@ngrx/store';
-// import * as TicketInfo from '../../../flight-search/store/selectors/tiket.selector';
 import { CartListInterface } from '../cart.model';
 
 export const selectCartFeature =
